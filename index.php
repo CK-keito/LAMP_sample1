f21d50c7f14bff8140ff79c810e789d4c020ff58
CK-keito <keito.chiba.sys19@morijyobi.ac.jp>
2020-06-18 11:57:25 +0900

    add index.html

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
 <?php
 $dsn = 'mysql:dbname=user;host=localhost;';
 $user = 'keito';
@@ -21,15 +13,11 @@ try {
     exit();
 }
 ?>
-    
-</body>
-</html>
 
-
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
@@ -43,3 +31,4 @@ try {
     
 </body>
 </html>
+
