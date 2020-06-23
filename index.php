8706029724f5c5d1cc697859cf05732660eebd80
CK-keito <keito.chiba.sys19@morijyobi.ac.jp>
2020-06-23 12:41:40 +0900

    edit6 php

--- index.php
+++ index.php
@@ -94,15 +94,15 @@ try {
 
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label" for="name">Name</label>
-                        <div class="col-sm-10>"
-                    <input class="form-control" type="text" name="name" id="name" required>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="name" id="name" required>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label" for="age">Age</label>
-                        <div class="col-sm-10>"
-                    <input class="form-control" type="text" name="age" id="age" required>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="age" id="age" required>
                         </div>
                     </div>
             
