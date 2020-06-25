14c766aa38f9c84c23c35353944d2e8654a604c6
CK-keito <keito.chiba.sys19@morijyobi.ac.jp>
2020-06-25 11:57:15 +0900

    php 6

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ try {
 
         <?php }
             else if($_GET['fg'] == 2){ ?>
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <strong>Failed</strong> Processiong could not be worked...
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
