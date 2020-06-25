127110ce4dfe67094bb304fcd33976f65c8db8e2
CK-keito <keito.chiba.sys19@morijyobi.ac.jp>
2020-06-25 11:33:33 +0900

    php 4

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'keito';
+$password = 'Kei0418s';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    $id = $_GET['id'];
+    
+
+    $sql = "delete from user where id = :id";
+    $stmt = $dbh->prepare($sql);
+    $params = array(':id' => $id);
+
+    $stmt->execute($parmas);
+    
+    header('Location: index.php?fg=1');
+
+    // print "接続成功\n";
+} catch (PDOException $e) {
+    // print "接続失敗: " . $e->getMessage() . "\n";
+    header('Location: index.php?fg=0?err=' . $e->getMessage());
+    exit();
+}
+?>
